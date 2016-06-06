7bbd45603dfbfb90533f943a67eebe3061646e8c
alumne <alexbonavila@iesebre.com>
2016-06-06 03:44:53 +0200

    bug fix part grafica

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,15 +9,15 @@
                 <form>
                     <div class="form-group">
                         <label for="User">User Name</label>
-                        <input type="text" class="form-control" placeholder={{ Auth::user()->name}} disabled>
+                        <input type="text" class="form-control" placeholder={{ Auth::user()->name}}>
                     </div>
                     <div class="form-group">
                         <label for="Email">Email address</label>
-                        <input type="email" class="form-control" placeholder={{ Auth::user()->email}} disabled>
+                        <input type="email" class="form-control" placeholder={{ Auth::user()->email}}>
                     </div>
                     <div class="form-group">
                         <label for="api_key">API key</label>
-                        <input type="text" class="form-control"  placeholder="{{ Auth::user()->apiKey->key }}" readonly>
+                        <input type="text" class="form-control"  placeholder="{{ Auth::user()->apiKey->key }}">
                     </div>
                 </form>
 @endsection
