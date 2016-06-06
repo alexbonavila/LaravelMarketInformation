28fe61d770bc90e4bbf8cf5d106b3150b7d67e8e
alumne <alexbonavila@iesebre.com>
2016-06-06 03:39:35 +0200

    refactoritzant AJAX + SweetAlert

--- resources/assets/js/save_calc.js
+++ resources/assets/js/save_calc.js
@@ -2,8 +2,6 @@ $('.form-calc-save button[type=submit]').click(function(e){
     e.preventDefault();
 
     $('#response div').remove();
-    $('#save_button').text(' Guardant...');
-    $('#save_button').append('<span id="loading" class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>');
 
     var formData = new FormData(document.getElementById("form-calc-save"));
 
@@ -18,16 +16,10 @@ $('.form-calc-save button[type=submit]').click(function(e){
         processData: false,
         data: formData,
         success: function(data) {
-            $('#loading').remove();
-            $('#save_button').text('Guarda');
-            $('#response').append('<div class="alert alert-info"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>El calcul ha sigut guardat correctament</div>')
-            console.log(data);
+            swal("Guardat amb exit");
         },
         error: function(data){
-            $('#loading').remove();
-            $('#save_button').text('Guarda');
-            $('#response').append('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>No hem pogut guardar el seu calcul</div>');
-            console.log(data);
+            swal("Error al guardar");
         }
     });
 });
